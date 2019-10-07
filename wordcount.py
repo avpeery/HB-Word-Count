eb07e1678230cd9a729adb9bec738a45416239a6
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-07 12:45:18 -0700

    Word Count Trial 2

--- wordcount.py
+++ wordcount.py
@@ -1,15 +1,28 @@
 # put your code here.
 def count_words(filename):
+    """Counts word occurences from a given string file  and returns dictionary
+
+        Input : File path
+
+        Output : dictionary of word counts (Returns dictionary)
+    """
     opened_file = open(filename)
     word_count_dict = {}
-    
+
     for line in opened_file:
         line = line.rstrip()
         words = line.split(" ")
 
         for word in words:
             word_count_dict[word] = word_count_dict.get(word, 0) + 1
-    
-    return word_count_dict
 
-print(count_words("test.txt"))
+    return word_count_dict
+    
+    
+
+def print_dictionary(given_dictionary):
+    for word, value in given_dictionary.items():
+        print(f"{word} {value}")
+
+
+print_dictionary(count_words("twain.txt"))
